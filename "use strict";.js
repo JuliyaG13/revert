989555f4f8fe6dc676f3cf85b07f0b800f62c3b8
Yuliya Gumitova <yulya1397@mail.ru>
2024-11-18 22:54:30 +0300

    Initialize project with npm package and .gitignore

---
+++ "use strict";.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function solveEquation(a, b, c) {
+    let arr = [];
+    const discriminant = Math.pow(b, 2) - 4 * a * c; 
+
+    if (discriminant < 0) {
+        
+        return arr; 
+    } else if (discriminant === 0) {
+        
+        const root = -b / (2 * a);
+        arr.push(root); 
+    } else {
+        
+        const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        arr.push(root1, root2); 
+    }
+
+    return arr; 
+}
